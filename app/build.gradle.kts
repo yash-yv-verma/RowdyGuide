f9b5ce6f60f0c9b7594cca562b270a17ca230d42
k-perrino <kaileyperrino@gmail.com>
2023-09-28 13:38:59 -0500

    Updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "edu.utsa.cs3443.rowdyguidefinal"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.utsa.cs3443.rowdyguidefinal"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -46,4 +46,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+
 }
