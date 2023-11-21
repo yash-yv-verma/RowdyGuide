c0a4bf05cd4b21fd10673d741f8de8ec5be0ff69
anjusoman <126534016+anjusoman@users.noreply.github.com>
2023-11-21 16:03:16 -0600

    Update MainActivity.java
    adding anju's part

--- app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/MainActivity.java
@@ -1,56 +1,40 @@
 package edu.utsa.cs3443.rowdyguidefinal;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import edu.utsa.cs3443.rowdyguidefinal.controller.MainController;
 import edu.utsa.cs3443.rowdyguidefinal.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    MainController mainController;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        Intent intent = getIntent();
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_events, R.id.navigation_calendar,R.id.navigation_map,R.id.navigation_profile)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);*/
-        // Initialize and assign variable
-        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
-        mainController = new MainController(this);
-        // Set Home selected
-        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+        if ( intent.getBooleanExtra( "logout", false ) ){
+            String toastText = "Successfully logged out.";
+            Toast.makeText( this, toastText, Toast.LENGTH_LONG ).show();
+        }
+        if ( intent.getBooleanExtra( "newAccountCreated", false ) ){
+            String toastText = "Successfully created account. Please log in.";
+            Toast.makeText( this, toastText, Toast.LENGTH_LONG ).show();
+        }
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> { // using lamda
-            mainController.onNavigationItemSelected(item); //call here
-            System.out.println("here");
-            return true;
-        });
-//        nav.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = nav.OnNavigationItemSelectedListener();
-        MenuItem item = bottomNavigationView.getMenu().findItem(bottomNavigationView.getSelectedItemId());
+        MainController mainController = new MainController(this);
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener( mainController );
 
+
+        Button createAccountButton = findViewById(R.id.createAccountButton);
+        createAccountButton.setOnClickListener( mainController );
 
     }
-
-
 }
