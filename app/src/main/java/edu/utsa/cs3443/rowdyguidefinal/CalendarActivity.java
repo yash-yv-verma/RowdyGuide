cef59c56178e94b24ea78016c6f1eba86062163a
k-perrino <kaileyperrino@gmail.com>
2023-11-21 17:10:18 -0600

    Changed event on calendar

--- app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/CalendarActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/CalendarActivity.java
@@ -34,8 +34,9 @@ public class CalendarActivity extends AppCompatActivity {
         Calendar cal = new Calendar();
         Event e = cal.makeCalendarEvent("name","date","time");
         StaticEvent.event = e;
-        cal.addEventToList(cal.makeCalendarEvent("test","22-11-2023","8am"));
-        cal.addEventToList(cal.makeCalendarEvent("test2","22-11-2023","8am"));
+        cal.addEventToList(cal.makeCalendarEvent("Science Test","22-11-2023","8am"));
+
+        cal.addEventToList(cal.makeCalendarEvent("Game night","22-11-2023","7pm"));
 
         cal.addEventToList(e);
         TextView calendarEvent = findViewById(R.id.eventListText);
