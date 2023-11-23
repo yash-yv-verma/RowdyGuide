d4666c65922ef73086cbe9cdfeafc38fb42b364b
Yash Verma <its_yash@icloud.com>
2023-11-22 18:58:29 -0600

    Changed the loading page.

--- app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/LoadingActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rowdyguidefinal/LoadingActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import edu.utsa.cs3443.rowdyguidefinal.R;
 
@@ -16,9 +19,19 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
         getSupportActionBar().hide();
 
+        // Get the ImageView for the loading image
+        ImageView loadingImage = findViewById(R.id.turtle);
+
+        // Load the rotation animation
+        Animation rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        // Start the animation
+        loadingImage.startAnimation(rotateAnimation);
+
         new Handler().postDelayed( new Runnable() {
             @Override
             public void run() {
+
                 // Start the main activity
                 Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
                 startActivity(intent);
@@ -26,7 +39,7 @@ public class LoadingActivity extends AppCompatActivity {
                 // Close the loading activity
                 finish();
             }
-        }, 2000);
+        }, 2500);
 
 
 
